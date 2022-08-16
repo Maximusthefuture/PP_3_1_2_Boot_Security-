eeae25dc23c4ccda12430348516daf553f5e86f1
MaximusTheFuture <naturemade@yandex.ru>
2022-08-16 11:32:17 +0300

    добавлен бутстрап

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/DbInit.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/DbInit.java
@@ -1,9 +1,13 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -11,10 +15,10 @@ import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Controller
-@RequestMapping("/")
+@Component
 public class InitialController {
 
     @Autowired
@@ -22,25 +26,22 @@ public class InitialController {
     @Autowired
     private RoleService roleService;
 
-    @GetMapping
-    String createAdminIfNotExists() {
+    @PostConstruct
+    void createAdminIfNotExists() {
         List<User> userList = userService.listUsers();
         Role role = new Role("ADMIN");
         Role userRole = new Role("USER");
-        if (userList.isEmpty()) {
-            PasswordEncoder encoder = new BCryptPasswordEncoder();
-            String password = encoder.encode("admin");
-            String userPassword = encoder.encode("user");
-            User admin = new User("admin", "Adminov", "admin@mail.ru",(byte) 30, password);
-            User user = new User("user", "Userov", "user@mail.ru", (byte) 30, userPassword);
-            roleService.add(role);
-            roleService.add(userRole);
-            admin.addRole(role);
-            admin.addRole(userRole);
-            user.addRole(userRole);
-            userService.add(admin);
-            userService.add(user);
-        }
-        return "index";
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = encoder.encode("admin");
+        String userPassword = encoder.encode("user");
+        User admin = new User("admin", "Adminov", "admin@mail.ru", (byte) 30, password);
+        User user = new User("user", "Userov", "user@mail.ru", (byte) 30, userPassword);
+        roleService.add(role);
+        roleService.add(userRole);
+        admin.addRole(role);
+        admin.addRole(userRole);
+        user.addRole(userRole);
+        userService.add(admin);
+        userService.add(user);
     }
 }
